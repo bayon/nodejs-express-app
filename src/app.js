20b3707dae515fc5aced75ee3cbd3610e4e5fbc4
Bayon Forte <bayon@forteworks.com>
2018-12-17 12:42:40 -0500

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express(); 
-/*
-app.use("/", (req,res,next)=>{
-    res.send("Welcome yo");
-});
-*/
+const appConfig = re	uire("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
+appConfig.init();
 routeConfig.init(app);
 module.exports = app;
